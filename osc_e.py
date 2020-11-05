372d2e330fdfb919d1af5295ce3fc3c44bcd89d9
Przemysław Danowski <36389823+phud-eppu@users.noreply.github.com>
2020-11-06 00:25:08 +0100

    nxosc to IEM SceneRotator

---
+++ osc_e.py
@@ -0,0 +1,28 @@
+# converter for nxosc to IEM SceneRotator Euler values 
+
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.udp_client import SimpleUDPClient
+
+def print_handler(address, *args):
+    print(f"{address}: {args}")
+
+ip_out = "127.0.0.1"
+port_out = 6500
+
+client = SimpleUDPClient(ip_out, port_out)  # Create client
+
+def default_handler(address, *argv): 
+    client.send_message("/SceneRotator/yaw", -argv[1])    # print("x: ", argv[0])
+    client.send_message("/SceneRotator/pitch", -argv[2])    # print("y: ", argv[1])
+    client.send_message("/SceneRotator/roll", -argv[0])    # print("z: ", argv[2])
+
+dispatcher = Dispatcher() 
+dispatcher.map("/nxosc/xyz", default_handler)
+""""dispatcher.set_default_handler(default_handler)"""
+
+ip_in = "127.0.0.1"
+port_in = 8000
+
+server = BlockingOSCUDPServer((ip_in, port_in), dispatcher)
+server.serve_forever()  # Blocks forever
